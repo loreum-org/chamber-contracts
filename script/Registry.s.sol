577ec1874d8736b25fbc1318c12504563cf6453b
Same Perkins <samperkinsdev@gmail.com>
2023-09-04 17:07:36 -0400

    chore: Update registry script

--- script/Registry.s.sol
+++ script/Registry.s.sol
@@ -3,16 +3,12 @@ pragma solidity 0.8.19;
 
 import "forge-std/Script.sol";
 import { Registry } from "../src/Registry.sol";
-import { DeployProxy } from "./utils/DeployProxy.sol";
 
 contract DeployRegistry is Script {
 
     function run() external {
         vm.startBroadcast();
-        bytes memory proxyData = abi.encodeWithSignature("initialize()");
-        Registry registry = new Registry();
-        DeployProxy registryProxy = new DeployProxy();
-        Registry(registryProxy.deploy(address(registry), proxyData));
+        new Registry();
         vm.stopBroadcast();
     }
 }
